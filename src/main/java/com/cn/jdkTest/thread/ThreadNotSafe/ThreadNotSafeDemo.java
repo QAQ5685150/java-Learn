63c0a9ad63c1edae06c0a5cd912e320557c7b26d
linxx <linxx@dtjoy.com>
2022-01-13 16:05:08 +0800

    多线程安全问题&生产者-消费者问题阻塞队列实现

--- src/main/java/com/cn/jdkTest/thread/ThreadNotSafe/ThreadNotSafeDemo.java
+++ src/main/java/com/cn/jdkTest/thread/ThreadNotSafe/ThreadNotSafeDemo.java
@@ -2,14 +2,15 @@ package com.cn.jdkTest.thread.ThreadNotSafe;
 
 /**
  * 类名:ThreadNotSafe
- * 描述:TODO
+ * 描述:多线程安全问题之卖票问题
  * 姓名:南风
  * 日期:2021-10-21 14:34
  **/
 
-public class ThreadNotSafeDemo implements Runnable{
+public class ThreadNotSafeDemo implements Runnable {
 
-    private static Integer ans = 0;
+    private static volatile Integer ans = 100;
+    private Object obj = new Object();
 
     @Override
     public void run() {
@@ -17,39 +18,37 @@ public class ThreadNotSafeDemo implements Runnable{
     }
 
     /**
-    *功能描述:多线程下不安全
-    *@return void
-    **/
-    public void getNext(){
-        for (int i = 0; i < 20000; i++) {
-            synchronized (this){
-                System.out.println(Thread.currentThread().getName());
-                System.out.println(ans++);
+     * 功能描述:多线程下不安全
+     * @return void
+     **/
+    public void getNext() {
+        //保持程序一直运行，也可以在开线程的地方for循环或者while 但是一定要有循环，不然程序只执行一次
+        //       while (ans > 0){
+        //同步代码块，加锁
+        synchronized (obj) {
+            //双重检查的应用，如果这里不判断，会出现票数为负的情况
+            //因为synchronized虽然锁住了线程，进入到锁的线程等待后如果没有第二重检查，就会让第一重检查的判断失效
+            if (ans > 0) {
+                try {
+                    //尝试等待，让每个线程都有机会抢票
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " ==>" + ans--);
             }
         }
     }
+    //   }
 
     public static void main(String[] args) {
         ThreadNotSafeDemo threadNotSafeDemo = new ThreadNotSafeDemo();
-
-        new Thread(() -> threadNotSafeDemo.run(),"thread-1").start();
-
-        new Thread(() -> threadNotSafeDemo.run(),"thread-2").start();
-
-        new Thread(() -> threadNotSafeDemo.run(),"thread-3").start();
-
-        new Thread(() -> threadNotSafeDemo.run(),"thread-4").start();
-
-//        for (int i = 0; i < 50; i++) {
-//            System.out.println(Thread.currentThread().getName());
-//
-//        }
-//        for (int i = 0; i < 50; i++) {
-//            System.out.println(Thread.currentThread().getName());
-//
-//        }
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> threadNotSafeDemo.run(), "thread-1").start();
+            new Thread(() -> threadNotSafeDemo.run(), "thread-2").start();
+            new Thread(() -> threadNotSafeDemo.run(), "thread-3").start();
+        }
 
     }
-
 }
 
--- src/main/java/com/cn/jdkTest/thread/lock/ProducerAndConsumer.java
+++ src/main/java/com/cn/jdkTest/thread/lock/ProducerAndConsumer.java
@@ -6,38 +6,44 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 类名:ProducerAndConsumer
- * 描述:TODO
+ * 描述:生产者消费者 阻塞队列实现
  * 姓名:南风
  * 日期:2021-10-19 11:37
  **/
 public class ProducerAndConsumer {
+
     private BlockingQueue<Integer> queue = new LinkedBlockingQueue<Integer>(10);
+
     class Producer extends Thread {
         @Override
         public void run() {
             producer();
         }
+
         private void producer() {
-            while(true) {
+            while (true) {
                 try {
                     queue.put(1);
                 } catch (InterruptedException e) {
+                    System.out.println(e);
                     e.printStackTrace();
                 }
                 System.out.println("生产者生产一条任务，当前队列长度为" + queue.size());
                 try {
-                    Thread.sleep(new Random().nextInt(1000)+500);
+                    Thread.sleep(new Random().nextInt(1000) + 500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
+
     class Consumer extends Thread {
         @Override
         public void run() {
             consumer();
         }
+
         private void consumer() {
             while (true) {
                 try {
@@ -47,13 +53,14 @@ public class ProducerAndConsumer {
                 }
                 System.out.println("消费者消费一条任务，当前队列长度为" + queue.size());
                 try {
-                    Thread.sleep(new Random().nextInt(1000)+500);
+                    Thread.sleep(new Random().nextInt(1000) + 500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
+
     public static void main(String[] args) {
         ProducerAndConsumer pc = new ProducerAndConsumer();
         Producer producer = pc.new Producer();
