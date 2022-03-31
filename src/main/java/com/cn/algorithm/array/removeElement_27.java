f9c5f2ec8d71e82779c129c3e94dd442a2322a05
小脑斧不可爱 <1820084219@qq.com>
2022-03-31 22:46:00 +0800

    股票问题  dp review 27.移除元素 双指针&滑动窗口类似技巧

---
+++ src/main/java/com/cn/algorithm/array/removeElement_27.java
@@ -0,0 +1,53 @@
+package com.cn.algorithm.array;
+
+/**
+ * @Auther: @Ð¡ÄÔ¸«²»¿É°®
+ * @Time: 2022-03-31 10:17
+ * @Description: TODO
+ * @Project_name: java-learn
+ */
+public class removeElement_27 {
+
+    public static void main(String[] args) {
+        int[] test = new int[]{0,1,2,2,3,0,4,2};
+        int i = removeElement_01(test, 2);
+        for (int j = 0; j < i; j++) {
+            System.out.print(test[j] + " ");
+        }
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int r = nums.length - 1;
+        while(nums[r] == val){
+            r--;
+        }
+        int index = 0;
+        for(int i = 0;i < r; i++){
+            if(nums[i] == val){
+                swap(nums,i,r);
+                r--;
+                index++;
+            }
+        }
+        return r + 1;
+    }
+
+    public static void swap(int[] nums,int l,int r){
+        int tmp = nums[l];
+        nums[l] = nums[r];
+        nums[r] = tmp;
+    }
+
+    public static int removeElement_01(int[] nums, int val) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
---
+++ src/main/java/com/cn/algorithm/gupiaomaimai/maxProfit_121.java
@@ -0,0 +1,28 @@
+package com.cn.algorithm.gupiaomaimai;
+
+/**
+ * @Auther: @Ð¡ÄÔ¸«²»¿É°®
+ * @Time: 2022-03-30 10:51
+ * @Description: TODO
+ * @Project_name: java-learn
+ */
+public class maxProfit_121 {
+
+    public static void main(String[] args) {
+        int[] test = new int[]{7,6,4,3,1};
+        System.out.println(maxProfit(test));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int len = prices.length;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            max = prices[i];
+            min = Math.min(min,prices[i]);
+            res = Math.max(res,max - min);
+        }
+        return res;
+    }
+}
