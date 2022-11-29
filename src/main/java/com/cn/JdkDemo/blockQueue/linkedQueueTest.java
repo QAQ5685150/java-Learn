4322482e8599f6f96db658b2d8b14bcd5d90f6d5
林啸轩 <linxx@samples.cngit>
2022-11-29 13:37:42 +0800

    jdk1.8 linkedQueue bug recurrent

---
+++ src/main/java/com/cn/JdkDemo/blockQueue/linkedQueueTest.java
@@ -0,0 +1,32 @@
+package com.cn.JdkDemo.blockQueue;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @Author: Linxx
+ * @Package: com.cn.JdkDemo.blockQueue
+ * @Time: 2022-11-28 13:29
+ * @Description: jdk1.8 linkedQueue bug recurrent,stream process linkedQueue while block current thread
+ **/
+public class linkedQueueTest {
+    public static void main(String[] args) throws Exception {
+        LinkedBlockingQueue<Object> queue = new LinkedBlockingQueue<>(1000);
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                while (true) {
+                    queue.offer(new Object());
+                    queue.remove();
+                }
+            }).start();
+        }
+        while (true) {
+            System.out.println("begin scan, i still alive");
+            queue.stream()
+                    .filter(o -> o == null)
+                    .findFirst()
+                    .isPresent();
+            Thread.sleep(100);
+            System.out.println("finish scan, i still alive");
+        }
+    }
+}
